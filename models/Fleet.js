98dd4000698cce0c1c36debe470df8526aba85b7
isaacBejarano <isaacbejaranopro@gmail.com>
2020-09-25 03:50:53 +0200

    FASE 2.1

--- models/Fleet.js
+++ models/Fleet.js
@@ -1,34 +1,23 @@
 "use strict";
-var Fleet = /** @class */ (function () {
-    function Fleet() {
-        this.fleet = [];
-    }
-    Object.defineProperty(Fleet.prototype, "getFleet", {
-        // getters
-        get: function () {
-            return this.fleet;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Fleet.prototype, "setFleet", {
-        // setters
-        set: function (fleet) {
-            this.fleet =
-                fleet.length >= 2
-                    ? fleet
-                    : [
-                        // prettier-ignore
-                        new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
-                        new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
-                    ];
-        },
-        enumerable: false,
-        configurable: true
-    });
-    // toString
-    Fleet.prototype.toString = function () {
-        console.log(this.fleet);
-    };
-    return Fleet;
-}());
+// class Fleet {
+// 	private fleet: Rocket[] = [];
+// 	// getters
+// 	get getFleet(): Rocket[] {
+// 		return this.fleet;
+// 	}
+// 	// setters
+// 	set setFleet(fleet: Rocket[]) {
+// 		this.fleet =
+// 			fleet.length >= 2
+// 				? fleet
+// 				: [
+// 						// prettier-ignore
+// 						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
+// 						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
+// 				  ];
+// 	}
+// 	// toString
+// 	toString(): void {
+// 		console.log(this.fleet);
+// 	}
+// }
--- models/Fleet.ts
+++ models/Fleet.ts
@@ -1,25 +1,25 @@
-class Fleet {
-	private fleet: Rocket[] = [];
+// class Fleet {
+// 	private fleet: Rocket[] = [];
 
-	// getters
-	get getFleet(): Rocket[] {
-		return this.fleet;
-	}
+// 	// getters
+// 	get getFleet(): Rocket[] {
+// 		return this.fleet;
+// 	}
 
-	// setters
-	set setFleet(fleet: Rocket[]) {
-		this.fleet =
-			fleet.length >= 2
-				? fleet
-				: [
-						// prettier-ignore
-						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
-						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
-				  ];
-	}
+// 	// setters
+// 	set setFleet(fleet: Rocket[]) {
+// 		this.fleet =
+// 			fleet.length >= 2
+// 				? fleet
+// 				: [
+// 						// prettier-ignore
+// 						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
+// 						new Rocket('Add 2 or more "Rockets" to the "Fleet"'),
+// 				  ];
+// 	}
 
-	// toString
-	toString(): void {
-		console.log(this.fleet);
-	}
-}
+// 	// toString
+// 	toString(): void {
+// 		console.log(this.fleet);
+// 	}
+// }
